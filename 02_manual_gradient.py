4440d68b61f04f9a1e69f4301a0a77c7d1b70993
Yumo Zhang <cmyumo.zhang@gmail.com>
2020-07-28 14:21:08 -0700

    update 02 loss function

--- 02_manual_gradient.py
+++ 02_manual_gradient.py
@@ -1,25 +1,21 @@
+import matplotlib.pyplot as plt
 # Training Data
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
 
 w = 1.0  # a random guess: random value
-
-
+w_lst = []
 # our model forward pass
 def forward(x):
     return x * w
 
-
 # Loss function
-def loss(x, y):
-    y_pred = forward(x)
-    return (y_pred - y) * (y_pred - y)
-
+def loss(y_pred, y_val):
+    return (y_pred - y_val) ** 2
 
 # compute gradient
 def gradient(x, y):  # d_loss/d_w
     return 2 * x * (x * w - y)
-
 
 # Before training
 print("Prediction (before training)",  4, forward(4))
@@ -33,8 +29,12 @@ for epoch in range(10):
         grad = gradient(x_val, y_val)
         w = w - 0.01 * grad
         print("\tgrad: ", x_val, y_val, round(grad, 2))
-        l = loss(x_val, y_val)
+        y_pre = forward(x_val)
+        l = loss(y_pre, y_val)
+        w_lst.append(w)
     print("progress:", epoch, "w=", round(w, 2), "loss=", round(l, 2))
 
 # After training
 print("Predicted score (after training)",  "4 hours of studying: ", forward(4))
+plt.plot(w_lst)
+plt.show()
